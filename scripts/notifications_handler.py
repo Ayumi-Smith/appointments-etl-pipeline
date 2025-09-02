9bfaadef9439fae3f76157f758dedc0cd451b95a
Ayumi-Smith <lyubovsztankulics@gmail.com>
2025-09-02 22:11:45 +0200

    added prototype of notification handling

---
+++ scripts/notifications_handler.py
@@ -0,0 +1,21 @@
+import requests
+
+#Very basic example of notifications sending, not requiring any additional setup.
+NOTIFICATION_URL = 'https://webhook.site/03075537-a273-451c-8f97-da4952dc434f'
+
+def send_file_failure_notification(filename, error_msg):
+    #msg = f'Error occurred handling file:  {filename}. {error_msg}'
+    msg = {
+        "status": "error",
+        "file": filename,
+        "message": error_msg,
+    }
+    requests.post(NOTIFICATION_URL, json=msg)
+
+def send_no_new_file_failure_notification():
+    #msg = 'ERROR: No new files were detected.'
+    msg = {
+        "status": "error",
+        "message": "No new files were found in the source."
+    }
+    requests.post(NOTIFICATION_URL, json=msg)
